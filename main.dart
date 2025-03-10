a01d8b4d1d3104399ecbd1e0d8054b04634b790e
Markus Willmann <markuswillmann87@gmail.com>
2025-03-10 14:53:25 +0100

    Aufgabe 1 erstellt

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
+  for (int i = 11; i < 60; i++) {
+    print(i);
+  }
+  for (int i = 61; i < 100; i++) {
+    print(i);
+  }
+}
