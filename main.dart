54c1b9e28ec4c087aed5e945fbebc6a8a980d8ef
Markus Willmann <markuswillmann87@gmail.com>
2025-03-11 13:38:01 +0100

    printanweisung korregiert und Werte geändert

--- main.dart
+++ main.dart
@@ -1,11 +1,23 @@
 void main() {
+  // for (int i = 0; i < 10; i++) {
+  //   print(i);
+  // }
+  // for (int i = 11; i < 60; i++) {
+  //   print(i);
+  // }
+  // for (int i = 61; i < 100; i++) {
+  //   print(i);
+  // }
+
   for (int i = 0; i < 10; i++) {
-    print(i);
+    print("Kleine Zahl: $i");
   }
-  for (int i = 11; i < 60; i++) {
-    print(i);
+
+  for (int i = 10; i < 60; i++) {
+    print("Mitlere Zahl: $i");
   }
-  for (int i = 61; i < 100; i++) {
-    print(i);
+
+  for (int i = 60; i < 100; i++) {
+    print("Große Zahl: $i");
   }
 }
